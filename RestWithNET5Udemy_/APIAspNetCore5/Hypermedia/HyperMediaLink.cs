d979cf8d800b09ccbd563cf687f932d166f06207
Bruno Baptista Pereira <bruno.baptistapereira@gmail.com>
2022-04-18 15:46:38 -0300

    HATEOS

---
+++ RestWithNET5Udemy_/APIAspNetCore5/Hypermedia/HyperMediaLink.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIAspNetCore5.Hypermedia
+{
+    public class HyperMediaLink
+    {
+        public string Rel { get; set; }
+
+        private string href;
+
+        public string Href
+        {
+            get
+            {
+                object _lock = new Object();
+                lock (_lock)
+                {
+                    StringBuilder sb = new StringBuilder(href);
+                    return sb.Replace("%2F", "/").ToString();
+                }
+            }
+            set
+            {
+                href = value;
+            }
+        }
+
+        public string Type { get; set; }
+
+        public string Action { get; set; }
+    }
+}
